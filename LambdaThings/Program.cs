04256a5bd23a79d3cde4a385c32ede38e6c5f8e5
visual studio <vs@itstep.ge>
2023-12-05 19:57:27 +0400

    Add project files.

---
+++ LambdaThings/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LambdaThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> names = new List<string>()
+            //{
+            //    "nika",
+            //    "mari",
+            //    "giorgi",
+            //    "pavle",
+            //    "andria"
+            //};
+
+            //foreach(string name in names)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+            //Console.WriteLine("---------------");
+
+            //names.ForEach((string name) => Console.Write(name + " "));
+
+            List<string> cities = new List<string>()
+            {
+                "D.C",
+                "Paris",
+                "Kuala-Lumpur",
+                "Providence",
+                "Tbilisi",
+                "Ontario"
+            };
+
+            cities.ForEach((string name) => Console.WriteLine(AddHello(name)));
+
+            static string AddHello(string name)
+            {
+                return $"{name}";
+            }
+        }
+    }
+}
